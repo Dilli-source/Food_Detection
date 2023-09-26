5bca9b4054a7ea15ef030b6ea2f2e7964223c35b
Dilli Ganesh <66422040+Dilli-source@users.noreply.github.com>
2023-09-26 13:12:57 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 13:03:56 2023
+​
+@author: dilli
+"""
+def main():
+    for _ in range(20):
+        print("Helloworld!!!!!!!")
+        
+        
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
